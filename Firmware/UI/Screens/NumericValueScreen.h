5a540189d4d9de9a84e920733e38c287dcda3e65
Sebastian Pichelhofer <sp-apertus@users.noreply.github.com>
2020-05-21 16:12:09 +0200

    new approach for T1202
    still needs checkbox icon though

--- Firmware/UI/Screens/NumericValueScreen.h
+++ Firmware/UI/Screens/NumericValueScreen.h
@@ -35,7 +35,7 @@ class NumericValueScreen : public IScreen
 
   public:
     explicit NumericValueScreen(IUSBDevice* usbDevice) :
-        IScreen(usbDevice), _cancelButton("Cancel"), _setButton("Set"), _liveButton("Live"),
+        IScreen(usbDevice), _cancelButton("Cancel"), _setButton("Set"), _liveButton("Live"), _homeButton((Icon*)&home),
         _header("Parameter Menu"), _value(0), _stepSize(1), _suffix("")
     {
         //_cancelButton.SetHandler(&CancelButtonHandler);
