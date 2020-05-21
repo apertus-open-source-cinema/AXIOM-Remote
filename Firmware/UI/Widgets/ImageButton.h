38a6538602838ac6345b5044e2a2df1c8c5a7ffc
Priya Pandya <priyapandya274@gmail.com>
2020-05-21 15:58:19 +0530

    -ImageButton now can have both image and text

--- Firmware/UI/Widgets/ImageButton.h
+++ Firmware/UI/Widgets/ImageButton.h
@@ -9,39 +9,77 @@
 class ImageButton : public IButton
 {
     Icon* _image;
+    const char* _label;
+    uint8_t _gap;
 
     uint8_t _cornerRadius;
     bool _highlighted;
 
     // Color Defintions
+    uint16_t _TextColor;
     uint16_t _imageColor;
     uint16_t _backgroundColor;
 
+    uint16_t _textHighlightColor;  
     uint16_t _imageHighlightColor;
     uint16_t _backgroundHighlightColor;
 
+    uint16_t _currentTextColor;
     uint16_t _currentImageColor;
     uint16_t _currentBackgroundColor;
 
   public:
-    explicit ImageButton(Icon* icon, uint16_t x = 0, uint16_t y = 0, uint16_t width = 0, uint16_t height = 0) :
-        IButton(x, y, width, height), _image(icon), _cornerRadius(3), _highlighted(false)
+    explicit ImageButton(Icon* icon, const char* caption = "", uint16_t x = 0, uint16_t y = 0, uint16_t width = 0, uint16_t height = 0) :
+        IButton(x, y, width, height), _image(icon), _cornerRadius(3), _highlighted(false), _label(caption)
     {
         _currentBackgroundColor = RGB565(220, 220, 220);
         _backgroundColor = _currentBackgroundColor;
+        _currentTextColor = _TextColor = (uint16_t)Color565::Black;
+      
+        _currentImageColor = _imageColor = (uint16_t)Color565::Black;
 
         _backgroundHighlightColor = (uint16_t)Color565::AXIOM_Blue;
+        _textHighlightColor = (uint16_t)Color565::Black;
+        _imageHighlightColor = (uint16_t)Color565::Black;
     }
 
     void SetCornerRadius(uint8_t cornerRadius)
     {
         _cornerRadius = cornerRadius;
     }
+  
+    void SetGap(uint8_t gap)
+    {
+        _gap = gap;
+    }
+  
+    uint8_t GetGap()
+    {
+        return _gap;
+    }
 
     virtual void Draw(IPainter* painter) override
     {
         painter->DrawFillRoundRectangle(_x, _y, _width, _height, _cornerRadius, _currentBackgroundColor);
-        painter->DrawIcon(_image->Data, _x + _width / 2 - _image->Width / 2, _y + _height / 2 - _image->Height / 2, _image->Width, _image->Height, _currentImageColor);
+        
+        if(_width > _height)
+        {
+            painter->SetFont(Font::FreeSans12pt7b);
+            
+            uint8_t totalWidth = _image->Width + GetGap() + painter->GetStringFramebufferWidth(_label);
+            uint8_t textPosY = _height / 2 + painter->GetCurrentFontHeight() / 2;
+            uint8_t imagePosX = _width / 2 - totalWidth / 2;
+            uint8_t textPosX = imagePosX + _image->Width + _gap;
+            
+            painter->DrawIcon(_image->Data, _x + imagePosX, _y + _height / 2 - _image->Height / 2, _image->Width, _image->Height, _currentImageColor);
+            painter->DrawText(_x + textPosX, _y + textPosY, _label, _currentTextColor, TextAlign::TEXT_ALIGN_LEFT,
+                              strlen(_label));
+        }
+      
+        else
+        {
+            painter->DrawIcon(_image->Data, _x + _width / 2 - _image->Width / 2, _y + _height / 2 - _image->Height / 2, _image->Width, _image->Height, _currentImageColor);
+        }
     }
 
     void SetBackgroundColor(uint16_t color)
@@ -53,6 +91,12 @@ class ImageButton : public IButton
     {
         _imageColor = color;
     }
+  
+    void SetTextColor(uint16_t color)
+    {
+        _TextColor = color;
+        SetHighlighted(_highlighted);
+    }
 
     void SetHighlightBackgroundColor(uint16_t color)
     {
@@ -63,6 +107,12 @@ class ImageButton : public IButton
     {
         _imageHighlightColor = color;
     }
+  
+    void SetHighlightTextColor(uint16_t color)
+    {
+        _textHighlightColor = color;
+        SetHighlighted(_highlighted);
+    }
 
     void SetHighlighted(bool highlighted)
     {
@@ -71,11 +121,13 @@ class ImageButton : public IButton
         {
             _currentImageColor = _imageHighlightColor;
             _currentBackgroundColor = _backgroundHighlightColor;
+            _currentTextColor = _textHighlightColor;
 
         } else
         {
             _currentImageColor = _imageColor;
             _currentBackgroundColor = _backgroundColor;
+            _currentTextColor = _TextColor;
         }
     }
 };
