d410f958afd448aa07b9ce95782f2a8975499975
Andrej <b.andrej@cumir.de>
2020-05-27 19:19:25 +0200

    - Fixed tests

--- FirmwareTest/Images/LogoIcon.h
+++ FirmwareTest/Images/LogoIcon.h
@@ -1,16 +1,3 @@
 #include <stdint.h>
 
-static const struct LogoIcon : Icon
-{
-    // uint16_t width;
-    // uint16_t height;
-    uint8_t data[10 * 6] = {0xFF, 0x03, 0x01, 0x02, 0x01, 0x02, 0x01, 0x02, 0x01, 0x02, 0xFF, 0x03};
-
-    LogoIcon()
-    {
-        Icon::Width = 10;
-        Icon::Height = 6;
-        Icon::Data = &data[0];
-    }
-
-} LogoIcon;
+const Icon LogoIcon = {10, 6, (const uint8_t[]){0xFF, 0x03, 0x01, 0x02, 0x01, 0x02, 0x01, 0x02, 0x01, 0x02, 0xFF, 0x03}};
