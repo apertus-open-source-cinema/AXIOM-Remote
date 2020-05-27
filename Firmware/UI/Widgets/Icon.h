c41b4cee906bb4fa22a4bed44bee7a81adee0f54
Andrej <b.andrej@cumir.de>
2020-05-27 15:42:31 +0200

    - Adjusted icon conversion

--- Firmware/UI/Widgets/Icon.h
+++ Firmware/UI/Widgets/Icon.h
@@ -3,17 +3,13 @@
 
 #include <inttypes.h>
 
-#define STATIC_SECTION __attribute__((space(prog), section(".kseg0_static_mem")))
+#define STATIC_SECTION __attribute__((section(".kseg0_static_data")))
 
 struct Icon
 {
     uint16_t Width;
     uint16_t Height;
     const uint8_t* Data;
-
-    // Icon(uint16_t width, uint16_t height, const uint8_t* data) : Width(width), Height(height), Data(data)
-    // {
-    // }
 };
 
 #endif // ICON_H
