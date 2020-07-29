73676cd4d4e7c252092618a1174b03848e01090d
panintended <63956419+panintended@users.noreply.github.com>
2020-07-29 23:52:50 +0300

    Observer pattern (T1203) (#20)
    * Infrastructure for T1203
    
    * Cleanup, alignment to project coding conventions

--- Firmware/CentralDB.h
+++ Firmware/CentralDB.h
@@ -1,82 +1,36 @@
 #ifndef CENTRALDB_H
 #define CENTRALDB_H
 
-#include <stdint.h>
-//#include <vector>
+#include "Attribute.h"
+#include "CentralDBObserver.h"
+#include "Utils.h"
+
+#include <cstdint>
+#include <cstring>
 
 class CentralDBObserver;
 
 class CentralDB
 {
-    // std::vector<class CentralDBObserver*> _views;
-    CentralDBObserver* _views[16];
-    uint8_t _viewscount = 0;
-    uint8_t _lcdBrightnessPercentage = 0;
+    struct ObserverAttributePair
+    {
+        CentralDBObserver* observer;
+        Attribute::ID attributeId;
+    };
+    static const int MAX_OBSERVER_ATTRIBUTE_PAIRS = 16;
+    static const int NUM_ATTRIBUTES = (int) Attribute::ID::NUM_ATTRIBUTES;
+
+    Attribute _attributes[NUM_ATTRIBUTES];
+    ObserverAttributePair _observerAttributePairs[MAX_OBSERVER_ATTRIBUTE_PAIRS];
+    uint8_t _observerAttributePairsCount;
 
   public:
-    CentralDB() : _views()
-    {
-        for (uint8_t i = 0; i < 16; i++)
-        {
-            _views[i] = nullptr;
-        }
-    }
+    CentralDB();
 
-    void attach(CentralDBObserver* obs)
-    {
-        //_views.push_back(obs);
-        _views[_viewscount++] = obs;
-    }
-
-    void SetLCDBrightness(uint8_t value)
-    {
-        _lcdBrightnessPercentage = value;
-        Notify();
-    }
-
-    uint8_t GetLCDBrightness()
-    {
-        return _lcdBrightnessPercentage;
-    }
-
-    void Notify();
-};
-
-class CentralDBObserver
-{
-    CentralDB* _db;
-
-  public:
-    explicit CentralDBObserver(CentralDB* db) : _db(db)
-    {
-    }
-
-    virtual void update() = 0;
-
-  protected:
-    CentralDB* GetDB()
-    {
-        return _db;
-    }
-};
-
-class TestObs : public CentralDBObserver
-{
-    uint8_t _value = 0;
-
-  public:
-    explicit TestObs(CentralDB* db) : CentralDBObserver(db)
-    {
-    }
-
-    void update()
-    {
-        _value = GetDB()->GetLCDBrightness();
-    }
-    uint8_t getvalue()
-    {
-        return _value;
-    }
+    void Attach(const Attribute::ID attributeId, CentralDBObserver* const observer);
+    const void* GetValue(Attribute::ID attributeId) const;
+    void SetValue(const Attribute::ID attributeId, const void* value);
+    void Notify(const Attribute::ID attributeId) const;
 };
 
 #endif /* CENTRALDB_H */
