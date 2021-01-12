af19e1d9e9dfa794e81091d7ef026be30a7e7afa
Andrej <b.andrej@cumir.de>
2021-01-12 23:58:40 +0100

    Removed std::move, which is not available in XC32

--- Firmware/CentralDBObserver.cpp
+++ Firmware/CentralDBObserver.cpp
@@ -2,8 +2,10 @@
 
 #include <utility>
 
+// TODO: Investigate -> Clang-Tidy: Pass by value and use std::move
+// NOTE: std::move is not available in XC32, reference: https://www.microchip.com/forums/m1089919.aspx
 CentralDBObserver::CentralDBObserver(const Attribute::ID attribureID, std::function<void(const CentralDB&)> callback) :
-    _observedAttributeId{attribureID}, _callback{std::move(callback)}
+    _observedAttributeId{attribureID}, _callback{callback}
 {
 }
 
