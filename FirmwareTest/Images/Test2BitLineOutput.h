433365ff2e297fee6258682396ebc43d33652f3d
Markus Engsner <markus.engsner@gmail.com>
2021-04-06 10:55:33 +0200

    Fix Draw2BitIcon tests

--- FirmwareTest/Images/Test2BitLineOutput.h
+++ FirmwareTest/Images/Test2BitLineOutput.h
@@ -14,15 +14,14 @@ constexpr float lowLerpThres = 0.3;
 constexpr float highLerpThres = 0.7;
 
 const std::map<uint8_t, uint16_t> noTransmapping{{0x0, Test2BitBGColor},
-                                                 {0x1, LerpColor(Test2BitBGColor, Test2BitFGColor, lowLerpThres)},
-                                                 {0x2, LerpColor(Test2BitBGColor, Test2BitFGColor, highLerpThres)},
+                                                 {0x1, AlphaBlend(Test2BitBGColor, Test2BitFGColor, 178)},
+                                                 {0x2, AlphaBlend(Test2BitBGColor, Test2BitFGColor, 76)},
                                                  {0x3, Test2BitFGColor}};
 
-const std::map<uint8_t, uint16_t> withTransMapping{
-    {0x0, Test2BitTransparentColor},
-    {0x1, LerpColor(Test2BitTransparentColor, Test2BitFGColor, lowLerpThres)},
-    {0x2, LerpColor(Test2BitTransparentColor, Test2BitFGColor, highLerpThres)},
-    {0x3, Test2BitFGColor}};
+const std::map<uint8_t, uint16_t> withTransMapping{{0x0, Test2BitTransparentColor},
+                                                   {0x1, AlphaBlend(Test2BitTransparentColor, Test2BitFGColor, 178)},
+                                                   {0x2, AlphaBlend(Test2BitTransparentColor, Test2BitFGColor, 76)},
+                                                   {0x3, Test2BitFGColor}};
 
 // The Test2Icon, with each pixel stored as an element.
 // Makes it easier to check the results.
