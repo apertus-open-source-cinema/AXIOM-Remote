0205bef556678858a5b0cfcbbbf23e81e14f973c
eppisai <theamanamanaman@gmail.com>
2021-04-09 19:03:39 +0530

    corrected path in Font definitions

--- Firmware/UI/FontDefinitions.h
+++ Firmware/UI/FontDefinitions.h
@@ -1,12 +1,12 @@
 #ifndef FONTDEFINITIONS_H
 #define FONTDEFINITIONS_H
 
-#include "UI/GFXFont.h"
+#include "GFXFont.h"
 
-#include "./Media/Fonts/FreeSans9pt7b.h"
-#include "./Media/Fonts/FreeSans12pt7b.h"
-#include "./Media/Fonts/FreeSans18pt7b.h"
-#include "./Media/Fonts/FreeSans24pt7b.h"
+#include "../Media/Fonts/FreeSans9pt7b.h"
+#include "../Media/Fonts/FreeSans12pt7b.h"
+#include "../Media/Fonts/FreeSans18pt7b.h"
+#include "../Media/Fonts/FreeSans24pt7b.h"
 
 enum class Font
 {
@@ -28,7 +28,7 @@ struct FontInfo{
     uint8_t fontHeight;
 };
 
-inline FontInfo fontList[4] = {
+FontInfo fontList[4] = {
     { FreeSans9pt7b, 13 },
     { FreeSans12pt7b, 17 },
     { FreeSans18pt7b, 25 },
