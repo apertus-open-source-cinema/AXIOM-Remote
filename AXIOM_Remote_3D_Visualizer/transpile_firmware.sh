ce742cb858aef8c426822b29927174974f7efe6c
Andrej <b.andrej@cumir.de>
2021-07-30 10:11:03 +0200

    - Cleaned up the build process

--- AXIOM_Remote_3D_Visualizer/transpile_firmware.sh
+++ AXIOM_Remote_3D_Visualizer/transpile_firmware.sh
@@ -22,6 +22,4 @@ SOURCES="FirmwareBinder/FirmwareBinder.cpp \
          ../Firmware/GlobalSettings.cpp"
 
          
-emsdk-run emcc -O2 -Wcast-align -Wover-aligned -fsanitize=undefined -s WARN_UNALIGNED=1 -s INITIAL_MEMORY=268435456 --bind -fexceptions -s ENVIRONMENT=web -s WASM=1 -s ASSERTIONS=1 -std=c++11 -s MODULARIZE=1 -s EXPORT_NAME="axiomRemoteFirmware" $SOURCES -o src/js/FW/axiom_remote_firmware.js
-#-fsanitize=address 
-#emsdk-run emcc --bind -s ASSERTIONS=1 -std=c++11 -s MODULARIZE=1 -s EXPORT_ES6=1 -s EXPORT_NAME="axiomRemoteFirmware" ../Firmware/TestClass.cpp ../Firmware/Painter/Painter.cpp ../Firmware/MenuSystem.cpp ../Firmware/Screens/MainPage.cpp ../Firmware/Screens/IScreen.cpp ../Firmware/Widgets/ImageButton.cpp -o js/FW/main.js
+emcc -O2 -Wcast-align -Wover-aligned -fsanitize=undefined -s WARN_UNALIGNED=1 -s INITIAL_MEMORY=268435456 --bind -fexceptions -s ENVIRONMENT=web -s WASM=1 -s ASSERTIONS=1 -std=c++11 -s MODULARIZE=1 -s EXPORT_NAME="axiomRemoteFirmware" $SOURCES -o src/js/FW/axiom_remote_firmware.js
